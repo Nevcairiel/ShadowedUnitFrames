627acc2e8ead188cc6d8726988b82b625abafaa6
exochron <exochron@gamerstate.de>
2021-01-09 20:04:05 +0100

    removed trailing whitespace in comment

--- modules/totems.lua
+++ modules/totems.lua
@@ -5,7 +5,7 @@ local MAX_TOTEMS = MAX_TOTEMS
 local playerClass = select(2, UnitClass("player"))
 if( playerClass == "DEATHKNIGHT" ) then
 	MAX_TOTEMS = 1
-	-- Unholy DKs get rank 2 on level 29 which converts their ghoul into a proper pet.  
+	-- Unholy DKs get rank 2 on level 29 which converts their ghoul into a proper pet.
 	local spec = (UnitLevel("player") < 29) and {1, 2, 3} or {1, 2}
 	ShadowUF:RegisterModule(Totems, "totemBar", ShadowUF.L["Totem bar"], true, "DEATHKNIGHT", spec, 12)
 elseif( playerClass == "DRUID" ) then
