afbe3456969d07d0bd7d80662b748c82c21fe8bd
Ssal <ysregist@gmail.com>
2024-08-17 18:13:42 +0900

    Update ShadowedUnitFrames.lua
    Set previous parents of frames before enter Editmode.

--- ShadowedUnitFrames.lua
+++ ShadowedUnitFrames.lua
@@ -702,6 +702,7 @@ local function hideBlizzardFrames(taint, ...)
 			frame:SetParent(ShadowUF.hiddenFrame)
 			frame:HookScript("OnShow", rehideFrame)
 		else
+			local old_parent = frame:GetParent()
 			EditModeManagerFrame:HookScript("OnShow", function(self)
 				frame:SetParent(old_parent)
 				end
